94370a404104677a2bc0cfb3cb9e6f031f2fa6cd
Mohammad Saif <saifsami321md@gmail.com>
2022-12-31 00:10:15 +0530

    project routes has been done

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,7 @@ const connection = require('./config')
 const userRouter = require('./Routes/user.routes')
 const ClientRouter = require('./Routes/Client.routes')
 const InvoiceRouter = require('./Routes/Invoice.routes')
+const ProjectRouter = require('./Routes/Project.routes')
 
 app.get('/',(req,res)=>{
     res.send('Welcome in Backend of hellobonsai!!')
@@ -18,6 +19,7 @@ app.get('/',(req,res)=>{
 app.use('/user',userRouter)
 app.use('/client',ClientRouter)
 app.use('/Invoice',InvoiceRouter)
+app.use('/project',ProjectRouter)
 
 const PORT = process.env.PORT || 8080
 
